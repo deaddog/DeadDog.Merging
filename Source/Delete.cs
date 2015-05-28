e790de0cbf5a9cdf069f17c123cd62da9b3cb9ab
deaddog <mikkelslsl@gmail.com>
2015-05-28 17:24:38 +0200

    Created implementations for changetypes Insert and Delete.

---
+++ Source/Delete.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DeadDog.Merging
+{
+    public class Delete<T> : Change<T>
+    {
+        private T value;
+        private int pos;
+        private Range range;
+
+        public Delete(T value, int pos, Range range)
+            : base(ChangeType.Deletion, value, pos, range)
+        {
+            this.value = value;
+            this.pos = pos;
+            this.range = range;
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+        public int Position
+        {
+            get { return pos; }
+        }
+        public Range Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Delete(\"{0}\", {2}, {1})", value, pos, range);
+        }
+    }
+}
---
+++ Source/Insert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DeadDog.Merging
+{
+    public class Insert<T> : Change<T>
+    {
+        private T value;
+        private int pos;
+        private Range range;
+
+        public Insert(T value, int pos, Range range)
+            : base(ChangeType.Insertion, value, pos, range)
+        {
+            this.value = value;
+            this.pos = pos;
+            this.range = range;
+        }
+
+        public T Value
+        {
+            get { return value; }
+        }
+        public int Position
+        {
+            get { return pos; }
+        }
+        public Range Range
+        {
+            get { return range; }
+            set { range = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Delete(\"{0}\", {2}, {1})", value, pos, range);
+        }
+    }
+}
