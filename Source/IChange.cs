ceeeb0e5e60df2a475a9f9d27bf3428821a3657a
deaddog <mikkelslsl@gmail.com>
2015-05-29 12:55:06 +0200

    The Change class is implemented as an interface; IChange.

---
+++ Source/IChange.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadDog.Merging
+{
+    public interface IChange<T>
+    {
+        IChange<T2> Clone<T2>(T2 newValue);
+
+        T Value
+        {
+            get;
+        }
+        int Position
+        {
+            get;
+        }
+        Range Range
+        {
+            get;
+        }
+    }
+}
---
+++ Source/IChangeExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadDog.Merging
+{
+    public static class IChangeExtension
+    {
+        public static void SortByPosition<T>(this List<IChange<T>> list)
+        {
+            list.Sort(ancestorPositionSort);
+        }
+        private static int ancestorPositionSort<T>(IChange<T> a, IChange<T> b)
+        {
+            return ancestorPosition((dynamic)a).CompareTo(ancestorPosition((dynamic)b));
+        }
+
+        private static int ancestorPosition<T>(Delete<T> delete)
+        {
+            return delete.Range.Start;
+        }
+        private static int ancestorPosition<T>(Insert<T> delete)
+        {
+            return delete.Position;
+        }
+        private static int ancestorPosition<T>(Move<T> delete)
+        {
+            return delete.Position1;
+        }
+    }
+}
