d57c1aca008d5f20b743245880784dff0c75a4e5
deaddog <mikkelslsl@gmail.com>
2015-05-29 13:40:46 +0200

    Moved the ancestor sorting to the ChangeQueue.

--- Source/ChangeQueue.cs
+++ Source/ChangeQueue.cs
@@ -8,5 +8,24 @@ namespace DeadDog.Merging
 {
     public class ChangeQueue
     {
+        #region Initial sorting
+        private static int ancestorPositionSort<T>(IChange<T> a, IChange<T> b)
+        {
+            return ancestorPosition((dynamic)a).CompareTo(ancestorPosition((dynamic)b));
+        }
+
+        private static int ancestorPosition<T>(Delete<T> delete)
+        {
+            return delete.Range.Start;
+        }
+        private static int ancestorPosition<T>(Insert<T> delete)
+        {
+            return delete.Position;
+        }
+        private static int ancestorPosition<T>(Move<T> delete)
+        {
+            return delete.Position1;
+        }
+        #endregion
     }
 }
