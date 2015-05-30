4ef401b36b69aec2d359350abf81de52a070e408
deaddog <mikkelslsl@gmail.com>
2015-05-30 12:51:45 +0200

    Created an interface IDiff for representing different diff methods.

---
+++ Source/IDiff.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace DeadDog.Merging
+{
+    public interface IDiff<T>
+    {
+        IEnumerable<IChange<T[]>> Diff(IEnumerable<T> origin, IEnumerable<T> modified);
+    }
+}
