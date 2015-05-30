575e9c7dcb2a0e56c127569e04605cedee4056bb
deaddog <mikkelslsl@gmail.com>
2015-05-30 15:49:35 +0200

    Created an interface for determining if a delete and an insert corresponds to a move.

---
+++ Source/IMoveIdentifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadDog.Merging
+{
+    public interface IMoveIdentifier<T>
+    {
+        double? MoveWeight(Delete<T> delete, Insert<T> insert);
+    }
+}
