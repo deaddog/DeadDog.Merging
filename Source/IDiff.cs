35e94ca2f7cf164f02e01a3284b8474e2db2701b
deaddog <mikkelslsl@gmail.com>
2015-05-31 17:14:57 +0200

    IDiff<T> requires T to be IEquatable<T>.

--- Source/IDiff.cs
+++ Source/IDiff.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace DeadDog.Merging
 {
-    public interface IDiff<T>
+    public interface IDiff<T> where T : IEquatable<T>
     {
         IEnumerable<IChange<T>> Diff(IEnumerable<T> origin, IEnumerable<T> modified);
     }
