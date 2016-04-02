50ab5e17975c7d87d0a33b2fc52dc069200a326d
deaddog <mikkelslsl@gmail.com>
2016-04-02 07:51:28 +0200

    Created a class to represent merge-conflicts.

---
+++ Source/Conflict.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadDog.Merging
+{
+    public class Conflict<T>
+    {
+        private IChange<T> a, b;
+        private string message;
+
+        public Conflict(IChange<T> a, IChange<T> b, string message)
+        {
+            this.a = a;
+            this.b = b;
+            this.message = message;
+        }
+
+        public IChange<T> A
+        {
+            get { return a; }
+        }
+        public IChange<T> B
+        {
+            get { return b; }
+        }
+
+        public string Message
+        {
+            get { return message; }
+        }
+    }
+}
