d735fde9f5438ff7d990372997393373161ee3e8
deaddog <mikkelslsl@gmail.com>
2018-11-05 23:13:55 +0100

    Defined tests for Range operations.

--- DeadDog.Merging.Tests/MergeTests.cs
+++ DeadDog.Merging.Tests/MergeTests.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DeadDog.Merging;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace DeadDog.Merging.Tests
 {
     [TestClass()]
---
+++ DeadDog.Merging.Tests/RangeTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DeadDog.Merging.Tests
+{
+    [TestClass]
+    public class RangeTests
+    {
+        [TestMethod]
+        public void ContainsSelfTest()
+        {
+            var zero = new Range(0, 0);
+            var one = new Range(1, 1);
+            var zeroTwo = new Range(0, 2);
+
+            Assert.IsTrue(zero.Contains(zero, includeStart: true));
+            Assert.IsTrue(one.Contains(one, includeStart: true));
+            Assert.IsTrue(zeroTwo.Contains(zeroTwo, includeStart: true));
+
+            Assert.IsFalse(zero.Contains(zero, includeStart: false));
+            Assert.IsFalse(one.Contains(one, includeStart: false));
+            Assert.IsFalse(zeroTwo.Contains(zeroTwo, includeStart: false));
+        }
+
+        [TestMethod]
+        public void ContainsUnitTest()
+        {
+            var zeroTwo = new Range(0, 2);
+
+            Assert.IsTrue(zeroTwo.Contains(new Range(0, 0), includeStart: true));
+            Assert.IsTrue(zeroTwo.Contains(new Range(1, 1), includeStart: true));
+            Assert.IsTrue(zeroTwo.Contains(new Range(2, 2), includeStart: true));
+
+            Assert.IsFalse(zeroTwo.Contains(new Range(0, 0), includeStart: false));
+            Assert.IsTrue(zeroTwo.Contains(new Range(1, 1), includeStart: false));
+            Assert.IsTrue(zeroTwo.Contains(new Range(2, 2), includeStart: false));
+        }
+
+        [TestMethod]
+        public void OverlapsSelfTest()
+        {
+            var zero = new Range(0, 0);
+            var one = new Range(1, 1);
+            var zeroTwo = new Range(0, 2);
+
+            Assert.IsTrue(zero.OverlapsWith(zero));
+            Assert.IsTrue(one.OverlapsWith(one));
+            Assert.IsTrue(zeroTwo.OverlapsWith(zeroTwo));
+        }
+
+        [TestMethod]
+        public void OverlapsUnitTest()
+        {
+            var zeroTwo = new Range(0, 2);
+
+            Assert.IsTrue(zeroTwo.OverlapsWith(new Range(0, 0)));
+            Assert.IsTrue(zeroTwo.OverlapsWith(new Range(1, 1)));
+            Assert.IsFalse(zeroTwo.OverlapsWith(new Range(2, 2)));
+        }
+
+        [TestMethod]
+        public void JoinSelfTest()
+        {
+            var zero = new Range(0, 0);
+            var one = new Range(1, 1);
+            var zeroTwo = new Range(0, 2);
+
+            Assert.AreEqual(zero, Range.Join(zero, zero));
+            Assert.AreEqual(one, Range.Join(one, one));
+            Assert.AreEqual(zeroTwo, Range.Join(zeroTwo, zeroTwo));
+        }
+
+        [TestMethod]
+        public void JoinUnitTest()
+        {
+            var zeroTwo = new Range(0, 2);
+
+            Assert.AreEqual(zeroTwo, Range.Join(zeroTwo, new Range(0, 0)));
+            Assert.AreEqual(zeroTwo, Range.Join(zeroTwo, new Range(1, 1)));
+            Assert.AreEqual(zeroTwo, Range.Join(zeroTwo, new Range(2, 2)));
+        }
+    }
+}
