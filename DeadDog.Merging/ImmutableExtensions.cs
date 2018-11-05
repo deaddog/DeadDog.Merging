e3cb64904f992f9be60904ca0daea0df7a980a74
deaddog <mikkelslsl@gmail.com>
2018-11-05 10:31:53 +0100

    Replace array extensions with immutable list extensions.

---
+++ DeadDog.Merging/ImmutableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+
+namespace DeadDog.Merging
+{
+    internal static class ImmutableExtensions
+    {
+        public static IImmutableList<T> GetRange<T>(this IImmutableList<T> list, int index, int count)
+        {
+            if (list is ImmutableList<T> actual)
+                return actual.GetRange(index, count);
+            else
+                return list.Skip(index).Take(count).ToImmutableList();
+        }
+        public static IImmutableList<T> GetRange<T>(this IImmutableList<T> list, int index)
+        {
+            return GetRange(list, index, list.Count - index);
+        }
+
+        public static IImmutableList<T> GetRange<T>(this IImmutableList<T> list, Range range)
+        {
+            return GetRange(list, range.Start, range.Length);
+        }
+    }
+}
