3ef74f81eb08b7dd69c2e8ec0a84c41e342ba436
deaddog <mikkelslsl@gmail.com>
2018-11-08 09:50:28 +0100

    Renamed Diff delegate 'Difference'.

--- DeadDog.Merging/Difference.cs
+++ DeadDog.Merging/Difference.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 
 namespace DeadDog.Merging
 {
-    public delegate IEnumerable<IChange<T>> Diff<T>(IEnumerable<T> a, IEnumerable<T> b);
+    public delegate IEnumerable<IChange<T>> Difference<T>(IImmutableList<T> sourceA, IImmutableList<T> sourceB);
 }
